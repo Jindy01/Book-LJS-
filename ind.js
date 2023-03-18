975490056e44e2caffbe998a8672d8ab3523c548
Jindy01 <125122479+Jindy01@users.noreply.github.com>
2023-03-18 23:04:23 +0300

    Выполнение доп заданий

---
+++ ind.js
@@ -0,0 +1,326 @@
+"use strict";
+
+let x = +prompt('Введите число больше 100');
+
+outer:
+if (x >= 0 && x <= 100) {
+    for (;;) {
+       x = +prompt('Введите верное значение');  }
+} else {
+    for (;x <= 0 && x >= 100;) break outer;
+}
+
+// Доделать цикл который будет вводит себя верное значение и прекращать другой цикл //
+
+
+
+
+// let x = 0;
+
+// while(x <= 3) {
+//     alert(`number ${x}`)
+//     ++x;
+// }
+
+
+
+
+// let x = 2;
+
+// for (x; x <= 10; x++) {
+//     if (x % 2 == 1 ) continue;
+
+//     alert(x);
+// }
+
+
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 == 0) continue;
+//     alert(i);
+// }
+
+
+
+// let p = 4312;
+// let i = prompt('Pass')
+
+// for (; i != p ;) {
+//     prompt('Pass')
+// }
+
+
+
+
+// let i = 0;
+// while (i < 3) { // выводит 0, затем 1, затем 2
+//   alert( i );
+
+// }
+
+
+
+
+// let z = 1;
+// let x = null;
+// let c = undefined;
+
+// alert(c ?? x);
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------//
+// // SQL //
+// let p;
+// let l;
+// let pass;
+// let login;
+// login = prompt('login');
+// pass = prompt('Pass');
+// l = 'A';
+// p = 1;
+// // SQL // 
+
+// if (login === l && pass >= p && pass <= p) {
+//     alert('Здраствуйте!');
+// } else if (pass != p && pass != '' && pass != null) {        
+//     alert('Неверный пароль!');
+// } else if (login != l && login != '' && login != null) {
+//     alert('Такого логина нету!');
+// } else if (login == '' || login == null ) {   
+//     alert('Не введён логин');
+// } else if (pass == '' || pass == null) {     
+//     alert('Не введён пароль');
+// }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+// Не дописана структура проверки на нулевой логин и пароль // ('::')
+
+
+// let x = -1 ;
+
+// alert(Boolean(x));
+
+
+
+
+// let age;
+// age = prompt('Возраст?');
+
+// if (age >= 14 && age <= 90) {
+//     alert('Нужный возраст');
+// } else {
+//     (!(age >= 14 && age <= 90)) 
+//     alert('Ненужный возраст');
+// }
+
+
+
+
+// alert( 1 && null && 2 );
+
+
+
+// let x = 0;
+// let z = 1;
+// let c = 1;
+
+// if (c >= z && z == c && c > 0) {
+//     alert('Read!')
+// }
+
+
+
+
+// let currentUser = null;
+// let defaultUser = "John";
+
+// let name = "unnamed" || defaultUser || currentUser;
+
+// alert(name);
+
+
+
+
+
+// let login;
+// login = prompt('Логин ?');  
+
+// let =    result  =   (login == 'Сотрудник') ? 'Hello!':
+//         (login == 'Директор')  ? 'Hellos!':
+//         (login == 'Начальник') ? 'Helloss!':
+//         (login == 'Служащий')  ? 'Hellosss!':'Неверный логин!';
+// alert(result);
+
+
+
+// let result;
+
+// let a = 2;
+// let b = 1;
+
+// let retro = (a + b < 4 ) ? 'Много' : 'Мало';
+// console.log(retro)
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------//
+// let numb = prompt('Введите число');
+
+// if (numb > 0 || numb <= 0) {
+//     alert('Это число');     
+// } else {
+//         (numb == String()); {
+//             alert('Это не число');
+//         }
+// }
+
+// if (numb > 0 ) {
+//     alert('Это значение больше нуля')
+// }  else if (numb < 0 ) {
+//         alert('Это значение меньше нуля')   
+//     } else if (numb == 0) {
+//             alert('Это нулевое значение ')
+//         } else {
+//                 (numb == String()); 
+//                     alert('Введите число!');
+// }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+// let text = prompt('Официальное название JS?');
+
+// if (text == 'ECMAScript') {
+//     alert('Да! Всё верно !')
+//     } else { 
+//     alert('Вы уверенны?')
+// }
+
+
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+// if (year < 2015) {
+//   alert( 'Это слишком рано...' );
+//     } else if (year > 2015) {
+//   alert( 'Это поздновато' );
+//     } else {
+//   alert( 'Верно!' );
+// }
+
+
+// let x = null;
+// let z = 0;
+
+// alert(x === z);
+
+
+// 5 > 4 //t
+// "ананас" > "яблоко" // f
+// "2" > "12" // t 
+// undefined == null // t
+// undefined === null // f 
+// null == "\n0\n"  // f 
+// null === +"\n0\n" // f 
+
+// 5 > 4 → true
+// "ананас" > "яблоко" → false
+// "2" > "12" → true
+// undefined == null → true
+// undefined === null → false
+// null == "\n0\n" → false
+// null === +"\n0\n" → false
+
+
+// console.log(0.1 + 0.2);
+// let x = null;
+// let z = undefined;
+
+// alert(z === x);
+// // why Folse ?
+
+
+
+// let a = 0;
+// alert( Boolean(a) ); // false
+
+// let b = "0";
+// alert( Boolean(b) ); // true
+
+// alert(a == b); // true!
+
+// let x = 0 ;
+// let z = '0';
+
+// console.log(Boolean(x));
+// console.log(Boolean(z));
+// console.log(x == z);
+
+
+// "" + 1 + 0 //10 -
+// "" - 1 + 0 //-1 +
+// true + false //1 +
+// 6 / "3" //2 +
+// "2" * "3" //6 +
+// 4 + 5 + "px"// 9px +
+// "$" + 4 + 5 //$45 +
+// "4" - 2 //2 +
+// "4px" - 2 //NaN -
+// "  -9  " + 5 //-9 5 -
+// "  -9  " - 5 // -14 +
+// null + 1 // 1 +
+// undefined + 1 //   NaN -
+// " \t \n" - 2 // -2 +
+
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+// alert(Number(a) + Number(b)); // 12
+
+// // const a = 1;
+// let b = 2;
+// let z = 3;
+
+// let f = 3 - (b = a + 2);
+// console.log(b)
+// console.log(f)
+// let z = 5;
+// console.log(z)
+// let z = (+'dfd');
+// console.log(typeof z)
+
+
+// let z = 2;
+
+// z = String();
+// console.log(typeof z);
+
+
+// // alert('2'%'2');
+
+
+// let x = NaN;
+// console.log(Number(x));
+
+// alert(Boolean(x));
+
+
+// let non = NaN
+// console.log(typeof non);
+// alert( String('gg') );
+
+// let njs = NaN;
+// alert(typeof(njs));
+
+// console.log('Conected!');
+
+// let text = prompt('Напишите инициалы');
+
+// alert (`${text} добро пожаловать !`)
