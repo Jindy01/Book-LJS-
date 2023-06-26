fa985f81586b5d01bfe22d5cf9c992be51c14e4b
Jindy01 <vip.fsafds@mail.ru>
2023-06-26 21:53:10 +0300

    closure

--- Lessons/Замыкание.js
+++ Lessons/Замыкание.js
@@ -1,11 +1,40 @@
 'use strict';
 
-function sum(x) {
-    sum(x) {
-        
+function bind() {
+    
+    return function logPerson() {
+        console.log(`Person: ${this.name} , age:${this.age}, job:${this.name}`);
     }
 }
 
+
+
+
+// function newNumb(n) {
+
+//     return function(x) {
+//         console.log(`Результат 1: ${20 * n + x}`);
+//     }
+// }
+
+// let numb = newNumb(2);
+// let twoNumb = newNumb(6);
+
+//     numb(10);
+//         twoNumb(2);
+//         twoNumb(10);
+
+
+// function newEnvi() {
+//     let name = 'Jony';
+
+//     return function getName() {
+//         alert(`Привет ${name}`);
+//     }
+// }
+
+// let names = newEnvi();
+//     names();
 
 
 // function test() {
