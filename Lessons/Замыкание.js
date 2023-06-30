af467a72ef2ec1a77f67c50e69e8b116a12dfca4
Jindy01 <vip.fsafds@mail.ru>
2023-06-30 19:40:57 +0300

    EndLesson_Extended_work_with_functions

--- Lessons/Замыкание.js
+++ Lessons/Замыкание.js
@@ -1,22 +1,21 @@
-'use strict';
 
-function makeArmy() {
-  let shooters = [];
+// function makeArmy() {
+//   let shooters = [];
 
-  for(let i = 0; i < 10; ++i) {
-    let shooter = function() { // функция shooter
-      alert( i ); // должна выводить порядковый номер
-    };
-    shooters.push(shooter);
-  }
+//   for(let i = 0; i < 10; ++i) {
+//     let shooter = function() { // функция shooter
+//       alert( i ); // должна выводить порядковый номер
+//     };
+//     shooters.push(shooter);
+//   }
 
-  return shooters;
-}
+//   return shooters;
+// }
 
-let army = makeArmy();
+// let army = makeArmy();
 
-army[0](); // у 0-го стрелка будет номер 10
-army[5](); // и у 5-го стрелка тоже будет номер 10
+// army[0](); // у 0-го стрелка будет номер 10
+// army[5](); // и у 5-го стрелка тоже будет номер 10
 
 
 
