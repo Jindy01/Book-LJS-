64f755637b1f48c5969da0e96bf05808d915fa36
Jindy01 <vip.fsafds@mail.ru>
2023-07-03 21:40:59 +0300

    WerryHardLesson

--- Lessons/Декораторы и переадресация вызова.js
+++ Lessons/Декораторы и переадресация вызова.js
@@ -1,32 +1,101 @@
 'use strict';
 
-
-function alert(x) {
-    alert(x);
+function f(a) {
+  console.log(a)
 }
 
-function debounce(f, ms) {
 
-let booleanParametr = true;
+function throttle(func, ms) {
+    let isThrottle = false,
+    savedArgs,
+    savedThis;
 
-setInterval(function() {
-  if (booleanParametr) {
-    booleanParametr = false;
-  } else {
-        booleanParametr = true;
-      }
-    }, 1000000000);
+    function wrapper() {
+        if (isThrottle) {
+            savedArgs = arguments;
+            savedThis = this;
+            return;
+        }
+    
+    func.apply(this, arguments);
+    
+    isThrottle = true;
 
-return function() {
-  setTimeout(() => f.apply(this, arguments), ms);
+    setTimeout(function() {
+        isThrottle = false;
+        if(savedArgs) {
+            wrapper.apply(savedThis, savedArgs);
+            savedArgs = savedThis = null;
+        }
+    }, ms);
+}
+return wrapper;
+}
 
-}};
+let f1000 = throttle(f, 1000);
 
-let f = debounce.bind('s', 1000);
+f1000(1); // показывает 1
+f1000(2); // (ограничение, 1000 мс ещё нет)
+f1000(3); // (ограничение, 1000 мс ещё нет)
 
-f(1); // выполняется немедленно
-f(2)
 
+
+// function alert(){
+//   console.log('hi');
+// }
+
+
+// function debounce(f, ms) {
+//   let isCalldown = false;
+
+//   return function() {
+//     if (isCalldown) return;
+
+//     f.apply(this, arguments);
+
+//     isCalldown = true;
+
+//     setTimeout(() => isCalldown = false, ms)
+//   }
+// }
+
+// let f = debounce(alert, 1000);
+
+// setTimeout( () => f(3), 100);
+// setTimeout( () => f(3), 1200);
+
+
+
+// function alerts() {
+//     console.log('hi');;
+// }
+
+// function debounce(f, ms) {
+
+// let booleanParametr = true;
+
+// setInterval(function() {
+//   if (booleanParametr) {
+//     booleanParametr = false;
+//   } else {
+//         booleanParametr = true;
+//       }
+//     }, ms);
+
+//     if (booleanParametr) {
+//       return function() {
+//         f.apply(this, arguments);
+//       }
+//     }
+// };
+
+
+
+// let f = debounce(alerts, 1000);
+
+// setTimeout( () => f(3), 100); 
+// setTimeout( () => f(4), 1100); 
+// setTimeout( () => f(5), 1500);
 
 
 // setTimeout(function() {
