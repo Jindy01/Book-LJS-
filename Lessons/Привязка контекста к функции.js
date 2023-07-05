22a69d6e2dd0bc84c96b5c92d3bf4a321e45f6fe
Jindy01 <vip.fsafds@mail.ru>
2023-07-05 19:13:15 +0300

    LessonForBind

--- Lessons/Привязка контекста к функции.js
+++ Lessons/Привязка контекста к функции.js
@@ -1,13 +1,59 @@
 'use strict';
 
-function mul(a, b) {
-    return a * b;
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
 }
 
-let double = mul.bind(null, 4);
+let user = {
+  name: 'Вася',
 
-console.log(double(4));
-console.log(double(8));
+  loginOk() {
+    alert(`${this.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+
+};
+  
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+
+
+
+// function partial(func, ...argsBound) {
+//     return function(...args) {
+//         return func.call(this, ...argsBound, ...args)
+//     }
+// }
+
+// let user = {
+//     firstName: 'Fob',
+//     say(time, phrase) {
+//         alert(`[${time}] : ${this.firstName} ${phrase}`)
+//     }
+// };
+
+// user.sayNow = partial(user.say, 
+//     new Date().getHours() + `:` +
+//     new Date().getMinutes());
+
+// user.sayNow('Hello');
+
+
+
+// function mul(a, b) {
+//     return a * b;
+// }
+
+// let double = mul.bind(null, 4);
+
+// console.log(double(4));
+// console.log(double(8));
+
 
 
 // let user = {
