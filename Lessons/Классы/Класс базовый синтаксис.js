112ea74d0135d5c16935f87f7ee92a7dff175d7a
Jindy01 <vip.fsafds@mail.ru>
2023-07-12 20:14:45 +0300

    Anding Lesson for class

--- Lessons/Классы/Класс базовый синтаксис.js
+++ Lessons/Классы/Класс базовый синтаксис.js
@@ -1,48 +1,86 @@
 'use strict';
 
-function Clock({ template }) {
+// function Clock({ template }) {
   
-    let timer;
+//     let timer;
   
-    function render() {
-      let date = new Date();
+//     function render() {
+//       let date = new Date();
   
-      let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
+//       let hours = date.getHours();
+//       if (hours < 10) hours = '0' + hours;
   
-      let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
+//       let mins = date.getMinutes();
+//       if (mins < 10) mins = '0' + mins;
   
-      let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
+//       let secs = date.getSeconds();
+//       if (secs < 10) secs = '0' + secs;
   
-      let output = template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
+//       let output = template
+//         .replace('h', hours)
+//         .replace('m', mins)
+//         .replace('s', secs);
   
-      console.log(output);
-    }
+//       console.log(output);
+//     }
   
-    this.stop = function() {
-      clearInterval(timer);
-    };
+//     this.stop = function() {
+//       clearInterval(timer);
+//     };
   
-    this.start = function() {
-      render();
-      timer = setInterval(render, 1000);
-    };
+//     this.start = function() {
+//       render();
+//       timer = setInterval(render, 1000);
+//     };
   
+//   }
+  
+// let clock = new Clock({template: 'h:m:s'});
+// console.log(clock.start());
+
+
+class MyTimer {
+  
+  
+  constructor( { template } ) {
+    this.template = template;
   }
-  
-let clock = new Clock({template: 'h:m:s'});
-console.log(clock.start());
+    
+  render() {
+    let date = new Date();
 
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
 
-class Timer {
-    //      Доделать !!!
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  };
+
+  start(){
+    this.render();
+      this.timer = setInterval(() => this.render(), 1000); //   Тут была ошибка ! Не возвращал значение через стрелочнуб Функцию
+  };
+
+  stop() {
+    clearInterval(this.timer);
+  };
+
 }
 
+let NewTimer = new MyTimer( {template: 'h:m:s'} );
+
+NewTimer.start();
+// NewTimer.stop();
 
 
 
