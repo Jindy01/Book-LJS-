20e4baf8e146d0a10bac503ed5bae12526d5ea91
Jindy01 <vip.fsafds@mail.ru>
2023-07-23 15:57:01 +0300

    NewLessonCallBack

--- Lessons/Обработка ошибок/Пользовательские ошибки, расширение Error.js
+++ Lessons/Обработка ошибок/Пользовательские ошибки, расширение Error.js
@@ -1,64 +1,132 @@
-'use strict';
+// class Error {
+//     constructor(massege) {
+//         this.massege = massege;
+//         this.name = `Error`;
+//     }
+// }
 
-class Error {
-    constructor(massege) {
-        this.massege = massege;
-        this.name = `Error`;
-    }
-}
+// class MyError extends Error {
+//     constructor(massege) {
+//         super(massege);
+//         this.name = this.constructor.name;
+//     }
+// }
 
-class MyError extends Error {
-    constructor(massege) {
-        super(massege);
-        this.name = this.constructor.name;
-    }
-}
+// class ValidationError extends Error { }
 
-class ValidationError extends MyError { }
+// class PropertyRequiredError extends ValidationError {
+//     constructor(property) {
+//         super(`Нет свойства:` + property);
+//         this.property = property;
+//     }
+// }
 
-class PropertyRequiredError extends ValidationError {
-    constructor(property) {
-        super(`Нет свойства:` + property);
-        this.property = property;
-    }
-}
+// class ReadError extends Error {
+//     constructor(massege, cause) {
+//         super(massege);
+//         this.cause = cause;
+//         this.name = 'ReadError';
+//     }
+// }
 
-console.log(new PropertyRequiredError('field').name);
+// function validateUser(user) {
+//     if (!user.age) {
+//         throw new PropertyRequiredError('age');
+//     }
 
-function readUser(json) {
-    let user = JSON.parse(json);
+//     if(!user.name) {
+//         throw new PropertyRequiredError('name');
+//     }
+// }
+// // console.log(new PropertyRequiredError('field').name);
 
-    if(!user.age) {
-        throw new PropertyRequiredError(`Нет поля: age`);
-    }
-    if(!user.name) {
-        throw new PropertyRequiredError(`Нет поля: name`);
-    }
-    return user;
-}
+// validateUser({name: 'Jony', age: 14,});
 
-try {
-    let user = readUser(`{"age": 25}`);
-} catch(err) {
-    if (err instanceof ValidationError) {
-        console.log(`Некоректные данные: ` + err.message);
-        console.log(err.name);
-        console.log(err.property);
-    } else if (err instanceof SyntaxError) {
-        console.log(`JSON Ошибка синтаксиса: `+ err.massege );
-    } else {
-        throw err;
-    }
-}
+// function readUser(json) {
+//     let user;
+
+// try {
+//     user = JSON.parse(json);
+// } catch(err) {
+//     if (err instanceof SyntaxError) {
+//         throw new ReadError('Синктаксическая ошибка', err);
+//     } else {
+//         throw err;
+//     }
+// }
+
+// try {
+//     validateUser(user)
+// } catch(err) {
+//     if (err instanceof ValidationError) {
+//         throw new ReadError('Ошибка валидации', err); 
+//     } else {
+//         throw err;
+//     }
+// }
+
+// try {
+//     readUser(`{bad json}`);
+// } catch(e) {
+//     if (e instanceof ReadError) {
+//         console.log(e);
+//         console.log(`Исходная ошибка ` + e.cause);
+//     } else {
+//         throw e;
+//     }
+// }
+
+
+// if(!user.age) {
+//     throw new PropertyRequiredError(`Нет поля: age`);
+// }
+// if(!user.name) {
+//     throw new PropertyRequiredError(`Нет поля: name`);
+// }
+//     return user;
+// }
+
+// class FormatError extends SyntaxError {
+//     constructor(message, stack) {
+//         super(stack);
+//         this.message = message;
+//         this.name = 'FormatError';
+//     }
+// }
+
+// let err = new FormatError('Ошибка форматирования'); 
+
+// console.log(err.message);
+// console.log(err.name);
+// console.log(err.stack);
+
+// console.log(err instanceof SyntaxError);
+// console.log(err instanceof FormatError);
+
+
+// let json = {
+//     name: 'Koll',
+//     age: 15,
+// }
+// let userS = JSON.stringify(json);
+// readUser(userS)
+
+
+
+// try {
+//     let user = readUser(`{"age": 25}`);
+// } catch(err) {
+//     if (err instanceof ValidationError) {
+//         console.log(`Некоректные данные: ` + err.message);
+//         console.log(err.name);
+//         console.log(err.property);
+//     } else if (err instanceof SyntaxError) {
+//         console.log(`JSON Ошибка синтаксиса: `+ err.massege );
+//     } else {
+//         throw err;
+//     }
+// }
         
-
-
-
-
-
-
-
-
 
 
 // function test() {
