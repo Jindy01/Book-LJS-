29fc001ac296e57bb848f1f453701f819e192600
Jindy01 <vip.fsafds@mail.ru>
2023-07-25 21:56:02 +0300

    MorePromise

--- Lessons/Промисы, async & await/Цепочка промисов.js
+++ Lessons/Промисы, async & await/Цепочка промисов.js
@@ -5,10 +5,36 @@ new Promise(function(resolve, reject) {
 }).then(function(result) {
     console.log(result);
     return result + 2;
+    
 }).then(function(result) {
     console.log(result);
     return result + 2;
+
 }).then(function(result) {
     console.log(result);
     return result + 2;
 });
+
+new Promise(function(resolve, reject) {
+    setTimeout(() => resolve(2), 1000);
+}).then(function(result) {
+    console.log(result);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(result + 2), 1000);
+    });
+}).then(function(result){
+    console.log(result);
+});
+
+
+
+//      YouTubeLesson
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Ready!')
+    }, 1000);
+    
+});
+
+promise.then((data) => console.log(data))   
