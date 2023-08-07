61518ef18e27b08ddaa7015806edfed571436e44
Jindy01 <vip.fsafds@mail.ru>
2023-08-07 14:10:40 +0300

    EndingBockOnJS

---
+++ Lessons/Разное/Intl интернационализация в JavaScript.js
@@ -0,0 +1,11 @@
+// 'use strict';
+
+// let animals = ["тигр", "ёж", "енот", "ехидна", "АИСТ", "ЯК"];
+
+// function arrIter(arr) {
+
+//         let collator = new Intl.Collator()
+//         console.log(arr.sort(collator.compare));
+// }
+
+// arrIter(animals);
--- Lessons/Разное/Каррирование.js
+++ Lessons/Разное/Каррирование.js
@@ -1,38 +1,36 @@
-'use strict';
+// function curry(f) {
+//     return function(a) {
+//         return function(b) {
+//             return f(a, b);
+//         };
+//     };
+// }
 
-function curry(f) {
-    return function(a) {
-        return function(b) {
-            return f(a, b);
-        };
-    };
-}
+// function sum(a, b) {
+//     return a + b;
+// }
 
-function sum(a, b) {
-    return a + b;
-}
+// let curriedSum = curry(sum);
 
-let curriedSum = curry(sum);
+// console.log(curriedSum(12)(2));
 
-console.log(curriedSum(12)(2));
+// function newCurry(func) {
 
-function newCurry(func) {
+//     return function curried(...args) {
+//         if (args.length >= func.length) {
+//             return func.apply(this, args);
+//         } else {
+//             return function pass(...args2) {
+//                 return curried.apply(this. args.concat(args2));
+//             }
+//         }
+//     };
+// }
 
-    return function curried(...args) {
-        if (args.length >= func.length) {
-            return func.apply(this, args);
-        } else {
-            return function pass(...args2) {
-                return curried.apply(this. args.concat(args2));
-            }
-        }
-    };
-}
+// function newSum(a, b, c) {
+//     return a + b + c;
+// }
 
-function newSum(a, b, c) {
-    return a + b + c;
-}
+// let curriedSumNew = newCurry(newSum);
 
-let curriedSumNew = newCurry(newSum);
-
-console.log(curriedSumNew(1, (3)));
+// console.log(curriedSumNew(1, (3)));
